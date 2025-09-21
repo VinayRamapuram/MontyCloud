d255633b265ea69760bf5ce91d79e9768a648a24
Ramapuram Vinay Goud <ramapuram.vinay.goud@continental-corporation.com>
2025-09-21 18:12:39 +0530

    python layer to install Pillow

--- stacks/s3_stack/s3_stack.py
+++ stacks/s3_stack/s3_stack.py
@@ -1,9 +1,9 @@
 import os
-from aws_cdk import (Stack, RemovalPolicy, Duration, aws_iam,  aws_s3 as s3, 
-                     aws_lambda as _lambda, aws_lambda_event_sources as events,
-                     aws_sqs as sqs, aws_s3_notifications as s3n,)
+from aws_cdk import (Stack, RemovalPolicy, Duration, aws_s3 as s3, 
+                     aws_lambda as _lambda, aws_sqs as sqs, aws_s3_notifications as s3n, aws_logs)
 from constructs import Construct
-
+from aws_cdk.aws_lambda_event_sources import SqsEventSource
+from aws_cdk.aws_lambda_python_alpha import PythonFunction, PythonLayerVersion
 from stacks.constants import IMAGES_S3_BUCKET_NAME, IMAGE_SERVICE_QUEUE_NAME, IMAGE_SERVICE_DLQ_QUEUE_NAME
 
 class S3Stack(Stack):
@@ -50,17 +50,33 @@ class S3Stack(Stack):
 
    
         
-    def img_lambda_processor(self, table):        
+    def img_lambda_processor(self, table):
+
+        lambda_layer = PythonLayerVersion(
+            self,
+            id=f"img-lambda-processor-layer",
+            layer_version_name=f"img-lambda-processor-layer",
+            entry=os.path.join(os.path.dirname(__file__), "lambdas", "python_layer"),            
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_11],
+            bundling={
+                    "image": _lambda.Runtime.PYTHON_3_11.bundling_image
+                }            
+        )  
+
         # Image Processor Lambda
-        img_processor_lambda = _lambda.Function(
+        img_processor_lambda =PythonFunction(
             self,  
-            f"{id}-image_processor_lambda",
+            f"image_processor_lambda",
             function_name="image_processor_lambda",
             description="Lambda function to Update image status",
             runtime=_lambda.Runtime.PYTHON_3_11,                      
-            code=_lambda.Code.from_asset(os.path.join(os.path.dirname(__file__), "lambdas")),
-            handler="image_processor.handler",
+            entry=os.path.join(os.path.dirname(__file__), "lambdas"),
+            index="image_processor.py",
+            architecture=_lambda.Architecture.X86_64,
+            layers=[lambda_layer],
             timeout=Duration.seconds(300),
+            log_retention=aws_logs.RetentionDays.ONE_MONTH,
+            memory_size=512,
             environment={
                 "IMAGES_TABLE": table.table_name,
                 "IMAGES_BUCKET": self.bucket.bucket_name,
@@ -75,10 +91,11 @@ class S3Stack(Stack):
 
 
         # S3 Event 
-        img_processor_lambda.add_event_source_mapping("SqsMapping",
-                                                    event_source_arn=self.queue.queue_arn,
-                                                    batch_size=10,
-                                                    enabled=True,
-                                                    max_batching_window=Duration.seconds(30))
+        img_processor_lambda.add_event_source(SqsEventSource(
+                self.queue,
+                batch_size=10,
+                max_batching_window=Duration.seconds(30)
+            )
+        )
         
         return img_processor_lambda
