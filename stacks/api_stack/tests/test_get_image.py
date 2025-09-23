b545a14854cd453876e891520d66fedafb636c7d
Ramapuram Vinay Goud <ramapuram.vinay.goud@continental-corporation.com>
2025-09-23 20:38:58 +0530

    fix lambda handler

--- stacks/api_stack/tests/test_get_image.py
+++ stacks/api_stack/tests/test_get_image.py
@@ -5,7 +5,7 @@ from stacks.api_stack.lambdas.download_image import handler as get_image_handler
 
 def test_get_image_not_found(aws_env):
     event = {"pathParameters": {"imageId": "nonexistent"}}
-    resp = get_image_handler.lambda_handler(event, None)
+    resp = get_image_handler(event, None)
     assert resp["statusCode"] == 404
 
 def test_get_image_success(aws_env):
@@ -14,7 +14,7 @@ def test_get_image_success(aws_env):
     item = {"PK":"user#u1","SK":"createdAt#t#i1","imageId":"i1","s3Key":"users/u1/i1/pic.jpg"}
     table.put_item(Item=item)
     event = {"pathParameters": {"imageId":"i1"}}
-    resp = get_image_handler.lambda_handler(event, None)
+    resp = get_image_handler(event, None)
     assert resp["statusCode"] == 200
     data = json.loads(resp["body"])
     assert "downloadUrl" in data
